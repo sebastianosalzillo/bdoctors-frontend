be24653cf7d518d5ffa9fee1588618f9c29945b4
AndreaRaga56 <andrearaga56@gmail.com>
2025-02-07 16:25:22 +0100

    feat axios pagina medico

--- src/pages/DettaglioMedico.jsx
+++ src/pages/DettaglioMedico.jsx
@@ -3,10 +3,26 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function DettaglioMedico() {
-  
 
-  return (
-    <h2>Dettagli medico</h2>
+  let { id } = useParams();
+  let [doc, setDoc] = useState(null)
+  useState(() => {
+    axios.get("http://localhost:3000/doctors/mario-rossi").then((resp) => {
+      setDoc(resp.data.data)
+      console.log(resp.data.data)
+    })
+  }, [])
+
+
+
+  return (<>
+    {doc &&
+      <>
+        <h2>Dettagli medico</h2>
+        <div>{doc.id}</div>
+      </>
+    }
+  </>
   );
 }
 
