eb0a4d870e69388cfd4c5ab6da0776c6d671d312
angeloalmanza <angeloalmanza26@gmail.com>
2025-02-08 12:58:16 +0100

    refactor: logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,18 +4,20 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link className="navbar-brand" to="/">BDoctors</Link>
-        
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <Link className="nav-link" to="/ricerca">Cerca Medici</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/registrazione">Registrati come Medico</Link>
-            </li>
-          </ul>
-        </div>
-      
+        <Link className="navbar-brand" to="/">
+          <img src="/logo_BDoctors.png" alt="Logo" width="75" height="75"/>
+        </Link>
+
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/ricerca">Cerca Medici</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/registrazione">Registrati come Medico</Link>
+          </li>
+        </ul>
+      </div>
+
     </nav>
   );
 }
