bde958a23d65c45347ea47e97f384c73c565e26b
AndreaRaga56 <andrearaga56@gmail.com>
2025-03-02 05:51:29 +0100

    fix errore nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,14 @@ function Navbar() {
     <nav className="navbar navbar-expand">
       <div className="container">
         {/* logo */}
-        <img src="/logo_BDoctors.png" alt="Logo" width="75" height="75" />
+        <div>
+          <img src="/logo_BDoctors.png" alt="Logo" />
+        </div>
 
         {/* lista dei link */}
-        <ul className="navbar-nav">
-          <li>
-            <Link className="nav-link" to="/">
-              <p>Home</p>
-            </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">Home</Link>
           </li>
           <li className="nav-item">
             <Link className="nav-link" to="/registration">Registrati come Medico</Link>
